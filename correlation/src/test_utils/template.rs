14282becf3bf753adab262b0c9f534a81b7fae27
Tibor Benke <ihrwein@gmail.com>
2016-04-12 16:47:12 +0200

    clarify Template related mock types
    Signed-off-by: Tibor Benke <ihrwein@gmail.com>

--- correlation/src/test_utils/template.rs
+++ correlation/src/test_utils/template.rs
@@ -20,41 +20,46 @@ impl Template for MockTemplate {
 }
 
 enum MockTime {
-    Format(Box<Fn() -> Result<MockTemplate, CompileError>>)
+    Format(Box<Fn(&str) -> MockTemplate>),
+    Compile(Box<Fn(&str) -> Result<MockTemplate, CompileError>>)
 }
 
 pub struct MockTemplateFactory (MockTime);
 
 impl MockTemplateFactory {
-    pub fn compile_error(error: &'static str) -> MockTemplateFactory {
-        MockTemplateFactory(MockTime::Format(Box::new(move || { Err(CompileError(error.to_owned())) })))
+    // returns the value which is compiled as an error
+    pub fn compile_error() -> MockTemplateFactory {
+        MockTemplateFactory(MockTime::Compile(Box::new(move |value| { Err(CompileError(value.to_owned())) })))
     }
-    pub fn literal(value: &'static str) -> MockTemplateFactory {
-        MockTemplateFactory(MockTime::Format(Box::new(move || {
+    // return a literal from format()
+    pub fn format_literal(value: &'static str) -> MockTemplateFactory {
+        MockTemplateFactory(MockTime::Format(Box::new(move |_| {
             let template = MockTemplate {
                 with_context: Box::new(move |_, _| { value }),
             };
-            Ok(template)
+            template
         })))
     }
-    pub fn context_id() -> MockTemplateFactory {
-        MockTemplateFactory::literal(CONTEXT_ID)
+    // returns the special context_id value from format
+    pub fn format_context_id() -> MockTemplateFactory {
+        MockTemplateFactory::format_literal(CONTEXT_ID)
     }
 }
 
 impl TemplateFactory<Message> for MockTemplateFactory {
     type Template = MockTemplate;
-    fn compile(&self, _: &str) -> Result<MockTemplate, CompileError> {
+    fn compile(&self, value: &str) -> Result<MockTemplate, CompileError> {
         match self.0 {
-            MockTime::Format(ref clojure) => clojure()
+            MockTime::Format(ref clojure) => Ok(clojure(value)),
+            MockTime::Compile(ref clojure) => clojure(value)
         }
     }
 }
 
 #[test]
 fn test_mock_template_factory_can_generate_errors() {
-    let factory = MockTemplateFactory::compile_error("ERROR");
+    let factory = MockTemplateFactory::compile_error();
     let expected = Err(CompileError("ERROR".to_owned()));
-    let actual = factory.compile("doesn't matter");
+    let actual = factory.compile("ERROR");
     assert_eq!(expected, actual);
 }
